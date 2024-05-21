3ef43db349202383181365ac9c1231157d4d5b7a
Beatriz Araujo <beatriz.a.0702@gmail.com>
2024-05-21 14:44:12 -0300

    feat: cria classe iphone e interfaces de telefone, navegador e reprodutor de audio

---
+++ src/Iphone.java
@@ -0,0 +1,55 @@
+import funcionalidades.AparelhoTelefonico;
+import funcionalidades.NavegadorDeInternet;
+import funcionalidades.ReprodutorMusical;
+
+public class Iphone implements AparelhoTelefonico, NavegadorDeInternet, ReprodutorMusical{
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+
+    @Override
+    public void ligar() {
+        System.out.println("Ligando...");
+    }
+
+    @Override
+    public void IniciarCorreioVoz() {
+        System.out.println("Iniciando correio de voz");
+    }
+
+    @Override
+    public void atender() {
+        System.out.println("Atender ligação");
+    }
+
+    @Override
+    public void selecionarMusica() {
+        System.out.println("Selecioanr música");
+    }
+
+    @Override
+    public void tocar() {
+        System.out.println("Tocar música");
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Pausar música");
+    }
+
+    @Override
+    public void atualizarPagina() {
+        System.out.println("Atualizar página");
+    }
+
+    @Override
+    public void exibirPagina() {
+        System.out.println("Exibir página");
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        System.out.println("Adicionar nova aba");
+        
+    }
+}
---
+++ src/funcionalidades/ReprodutorMusical.java
@@ -0,0 +1,11 @@
+package funcionalidades;
+
+/**
+ * ReprodutorMusical
+ */
+public interface ReprodutorMusical {
+    public void tocar();
+    public void pausar();
+    public void selecionarMusica();
+    
+}
